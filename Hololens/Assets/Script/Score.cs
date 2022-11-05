a9aafc961ad9b3ef133fa04600ac5d6ebad9b687
Jinho227 <sreika227@gmail.com>
2022-11-05 22:56:52 +0900

    add: score text

---
+++ Hololens/Assets/Script/Score.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text scoreText;
+    private int score = 0;
+
+    public void AddScore(int num)
+    {
+        score += num;
+        scoreText.text = "Score : " + score;
+    }
+
+}
